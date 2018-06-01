8907c69d15dbcd151148b32a8543a3f29e00cbeb
Ethan Cabbage <ethan.phyoke@gmail.com>
2018-06-01 10:05:38 -0700

    Add instructions comment

--- scripts/defaultAudioMonitor.bat
+++ scripts/defaultAudioMonitor.bat
@@ -1,5 +1,6 @@
 @echo off
 
 REM // This requires NirCmd (http://www.nirsoft.net/utils/nircmd.html)
+REM // Change the name of the speakers under Playback Devices > Properties
 
 nircmd setdefaultsounddevice "Ultrawide" 1
